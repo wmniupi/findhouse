10d97a603bd2fd61c601fa2ca6cd8e4b7eb2e637
‘王萌’ <1335665801@qq.com>
2021-12-07 18:38:18 +0800

    'wmover2'

--- js/setRem.js
+++ js/setRem.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
     function setRem() {
         var ui_w = 375;
@@ -7,12 +6,3 @@
         html_.style.fontSize = (clientWidth / ui_w) * 10 + 'px';
     }
     window.onresize = setRem;
-=======
-function setRem() {
-    var ui_w = 375;
-    var clientWidth = document.documentElement.clientWidth || document.body.clientWidth;
-    var html_ = document.getElementsByTagName('html')[0];
-    html_.style.fontSize = (clientWidth / ui_w) * 10 + 'px';
-}
-window.onresize = setRem;
->>>>>>> 9ca421af93015607ffecbc6fe4645fd9b2bc2ca0
